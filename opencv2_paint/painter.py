1805b018458cee1ffd79cb48a9425de3f56586ae
= <dharm1k987@gmail.com>
2020-07-28 19:11:54 -0400

    add original image for comparsion

--- opencv2_paint/painter.py
+++ opencv2_paint/painter.py
@@ -1,14 +1,46 @@
 import cv2
 import numpy as np
 
+# function that helps stack images so it stays in one window
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
 
-# function to convert a hsv color to rgb
+# function to convert a HSV color to rgb, hsvColor is given as [hue,sat,value]
 def hsvToBGR(hsvColor):
     return cv2.cvtColor(np.uint8([[hsvColor]]), cv2.COLOR_HSV2BGR)[0][0]
 
 
 # function to find the color of the marker given the image and return the set of points to add
-def findColor(img, markerColors, colorToShow):
+def findColor(img, markerColors, colorToShow, imgResult):
+    # convert image to HSV format
     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     newPoints = []
     # go through each marker color
@@ -20,7 +52,7 @@ def findColor(img, markerColors, colorToShow):
         # create a mask which only shows this color
         mask = cv2.inRange(imgHSV, lower, upper)
         # get the contours for this mask, which represent the marker lid
-        x, y, w, h = getContours(mask)
+        x, y, w, h = getContours(mask, imgResult)
         # create a circle around the top center point
         cv2.circle(imgResult, (x + w // 2, y), 15, colorToShow[i], cv2.FILLED)
         # if it was a valid contour, then add the points
@@ -32,7 +64,7 @@ def findColor(img, markerColors, colorToShow):
 
 
 # function to return the contours of the marker lid given an image
-def getContours(img):
+def getContours(img, imgResult):
     contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     x, y, w, h = 0, 0, 0, 0
     # there is only one contour we are after, the marker lid
@@ -41,6 +73,7 @@ def getContours(img):
         area = cv2.contourArea(cnt)
         perimeter = cv2.arcLength(cnt, closed=True)
 
+        # this should match the marker lid
         if area > 500:
             # approximate the location of the corner points and create a bounding box around it
             approx = cv2.approxPolyDP(cnt, 0.02 * perimeter, closed=True)
@@ -51,10 +84,11 @@ def getContours(img):
             return x, y, w, h
     return x, y, w, h
 
-
-def drawOnCanvas(myPoints, myColorValues):
-    for point in myPoints:
-        cv2.circle(imgResult, (point[0], point[1]), 10, myColorValues[point[2]], cv2.FILLED)
+# given a set of points [[x,y,colorId], [..], ...], draw circles on screen with the x,y and the color
+def drawOnCanvas(points, myColorValues, imgResult):
+    for point in points:
+        if point[0] != 0 and point[1] != 0:
+            cv2.circle(imgResult, (point[0], point[1]), 10, myColorValues[point[2]], cv2.FILLED)
 
 
 # webcam setup
@@ -82,19 +116,23 @@ colorToShow = [np.array(hsvToBGR([a, b + bgrShift, c + bgrShift])).tolist() for 
 points = []  # [[x,y,colorId], [..], ...]
 
 while True:
+    # read the current image, and flip it so its easy to paint
+    # create a copy which we will modify
     _, img = cap.read()
     img = cv2.flip(img, 1)
     imgResult = img.copy()
 
     # get the set of new points to add
-    newPoints = findColor(img, markerColors, colorToShow)
+    newPoints = findColor(img, markerColors, colorToShow, imgResult)
 
     # we have to extend the original points (not append), since newPoints is a list itself
     points.extend(newPoints)
 
-    drawOnCanvas(points, colorToShow)
+    drawOnCanvas(points, colorToShow, imgResult)
 
-    cv2.imshow('Result', imgResult)
+    cv2.imshow('Original and New', stackImages(0.8, [img, imgResult]))
+
+    # cv2.imshow('Result', imgResult)
 
     wait = cv2.waitKey(1)
     if wait & 0xFF == ord('q'):
@@ -102,7 +140,7 @@ while True:
     elif wait & 0xFF == ord('c'):
         # clear the screen
         points = []
-        drawOnCanvas(points, colorToShow)
+        drawOnCanvas(points, colorToShow, imgResult)
 
 cv2.destroyAllWindows()
 cap.release()
