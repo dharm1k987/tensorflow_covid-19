8744fc2083296d0d8f7bc41033e01ede891db42c
= <dharm1k987@gmail.com>
2020-07-28 14:21:45 -0400

    Added working version 1 of paint

---
+++ opencv2_paint/color_picker.py
@@ -0,0 +1,94 @@
+import cv2
+import numpy as np
+
+def empty(a):
+    pass
+
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
+
+
+cv2.namedWindow('TrackBars')
+# name should be the same
+cv2.resizeWindow('TrackBars', 640, 240)
+# name of trackbar, window it belongs to, intial starting value, max value, onChange method
+# these initial starting points match the color orange
+cv2.createTrackbar('Hue Min', 'TrackBars', 0, 179, empty)
+cv2.createTrackbar("Hue Max","TrackBars",0,179,empty)
+cv2.createTrackbar("Sat Min","TrackBars",0,255,empty)
+cv2.createTrackbar("Sat Max","TrackBars",0,255,empty)
+cv2.createTrackbar("Val Min","TrackBars",0,255,empty)
+cv2.createTrackbar("Val Max","TrackBars",0,255,empty)
+
+frameWidth = 640
+frameHeight = 480
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+# cap.set(10, 150)
+
+while True:
+
+    success, img = cap.read()
+
+    # convert img to HSV (hue sat variance)
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    # get the value in hue min and the rest of them
+    h_min = cv2.getTrackbarPos('Hue Min', 'TrackBars')
+    h_max = cv2.getTrackbarPos("Hue Max", "TrackBars")
+    s_min = cv2.getTrackbarPos("Sat Min", "TrackBars")
+    s_max = cv2.getTrackbarPos("Sat Max", "TrackBars")
+    v_min = cv2.getTrackbarPos("Val Min", "TrackBars")
+    v_max = cv2.getTrackbarPos("Val Max", "TrackBars")
+    print(h_min, h_max, s_min, s_max, v_min, v_max)
+
+    # create a mask that is in the range of these colors
+    # min and max range
+    lower = np.array([h_min,s_min,v_min])
+    upper = np.array([h_max,s_max,v_max])
+    # the mask should find the color that we want highlighted
+    mask = cv2.inRange(imgHSV, lower, upper)
+    imgResult = cv2.bitwise_and(img, img, mask=mask)
+
+    cv2.putText(img, 'Original', (0,50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 150, 0), 3)
+    cv2.putText(imgHSV, 'HSV', (0,50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 150, 0), 3)
+    cv2.putText(mask, 'Mask', (0,50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 3)
+    cv2.putText(imgResult, 'Result', (0,50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 150, 0), 3)
+
+    imgStack = stackImages(0.6,([img,imgHSV],[mask,imgResult]))
+    cv2.imshow("Stacked Images", imgStack)
+
+    if cv2.waitKey(1) and 0xF == ord('q'):
+        break
+
+cap.release();
+cv2.destroyAllWindows();
